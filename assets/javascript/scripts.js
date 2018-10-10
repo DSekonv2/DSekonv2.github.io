da90ebafcd2169edf057dfae0b686adfaa879d77
Serg7 <AnnSe7@mail.ru>
2018-10-11 02:54:38 +0300

    Carousel counter and fix

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -4,92 +4,22 @@ $('.m-slider').slick({
     slidesToScroll: 1
 });
 
-
-//$('.slider-carousel').on('init', function () {
-//    console.log($(".slider-carousel .s-c_card.slick-center").index());
-//    $(".slider-carousel .s-c_card").removeClass("slide-left slide-right");
-//    $(".slider-carousel .s-c_card").eq($(".slider-carousel .s-c_card.slick-center").index() - 1).addClass("slide-left");
-//    $(".slider-carousel .s-c_card").eq($(".slider-carousel .s-c_card.slick-center").index() + 1).addClass("slide-right");
-//});
-//$('.slider-carousel').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-//    
-//    if (currentSlide != nextSlide) {
-//        console.log(currentSlide + " " + nextSlide);
-//        $(".slider-carousel .s-c_card").removeClass("slide-left slide-right");
-//        $(".slider-carousel .s-c_card").eq($(".slider-carousel .s-c_card.slick-active.slick-center").index() - 1).addClass("slide-left");
-//        $(".slider-carousel .s-c_card").eq($(".slider-carousel .s-c_card.slick-active.slick-center").index() + 1).addClass("slide-right");
-//    }
-//});
-//
-//$('.slider-carousel').slick({
-//    infinite: true,
-//    initialSlide: 1,
-//    centerMode: true,
-//    draggable: false,
-//    slidesToShow: 1,
-//    variableWidth: true,
-//    slidesToScroll: 1
-//
-//});
-
-
-//$('.slider-carousel').slick({
-//    infinite: true, 
-//  centerMode: true,
-//    variableWidth: true,
-//  slidesToShow: 3,
-//  responsive: [
-//    {
-//      breakpoint: 768,
-//      settings: {
-//        arrows: false,
-//        centerMode: true,
-//        centerPadding: '40px',
-//        slidesToShow: 3
-//      }
-//    },
-//    {
-//      breakpoint: 480,
-//      settings: {
-//        arrows: false,
-//        centerMode: true,
-//        centerPadding: '40px',
-//        slidesToShow: 1
-//      }
-//    }
-//  ]
-//});
-
-//var glide = new Glide('.glide', {
-//    type: 'carousel',
-//    perView: 3,
-//    focusAt: 'center',
-//    breakpoints: {
-//        800: {
-//            perView: 2
-//        },
-//        480: {
-//            perView: 1
-//        }
-//    }
-//});
-//
-//glide.on(['mount.after', 'run.after'], function () {
-//    console.log(glide.index);
-//    $(".glide .glide__slide").removeClass("slide-left slide-right");
-//    $(".glide .glide__slide").eq($(".slider-carousel-block .glide__slide--active").index() - 1).addClass("slide-left");
-//    $(".glide .glide__slide").eq($(".slider-carousel-block .glide__slide--active").index() + 1).addClass("slide-right");
-//
-//});
-//
-//glide.mount();
 new Vue({
-  el: '#slider-carousel',
-  data: {
-    slides: 5
-  },
-  components: {
-    'carousel-3d': Carousel3d.Carousel3d,
-    'slide': Carousel3d.Slide
-  }
+    el: '#slider-carousel',
+    data: {
+        slides: $(".carousel-3d .s-c_card").length
+    },
+    created: function () {
+        $(".counter-carousel_general").html(this.slides);
+    },
+    methods: {
+        onBeforeSlideChange(index) {
+            $(".counter-carousel_current").html(index + 1);
+        }
+    },
+    components: {
+        'carousel-3d': Carousel3d.Carousel3d,
+        'slide': Carousel3d.Slide
+    }
+
 })
